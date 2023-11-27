71ac917032439b8f69758a39123773bdc8418411
Ygor Pinto Gama <ygorpintogama@pop-os.localdomain>
2023-11-26 21:35:32 -0300

    First steps with java spring-boot

---
+++ 05_FirstStepWithInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/ygorpgama/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.ygorpgama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
